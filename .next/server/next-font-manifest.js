9b05b07f26b8c933feba6136387e2a5403c09cd2
Roberto Israel Lopez <ilbroberto99316@gmail.com>
2026-04-09 21:51:06 -0500

    Remove unused type definitions and entry checks for dashboard and wedding pages

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/rilopez99316/Documents/Projects/wedding-planner/src/app/layout\":[\"static/media/7b89a4fd5e90ede0-s.p.woff2\",\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
